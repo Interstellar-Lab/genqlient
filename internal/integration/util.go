2fa4f14eb90ae9d3dd0c8dd48bce4d6b0514cfca
matthieu4294967296moineau <matthieu@interstellarlab.earth>
2023-10-12 16:55:28 +0200

    only il

--- internal/integration/util.go
+++ internal/integration/util.go
@@ -8,7 +8,7 @@ import (
 	"runtime"
 	"testing"
 
-	"github.com/Khan/genqlient/generate"
+	"github.com/Interstellar-Lab/genqlient/generate"
 )
 
 // RepoRoot returns the root of the genqlient repository,
